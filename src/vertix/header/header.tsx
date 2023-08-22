5f1b9efc460c0b4b4325cf8fd0176f068895cb8f
Leonid Vinikov <czf.leo123@gmail.com>
2023-08-22 05:49:33 +0300

    Fix: Sass - Normalize aiming for better practice

--- src/vertix/header/header.tsx
+++ src/vertix/header/header.tsx
@@ -75,11 +75,14 @@ export default function Header() {
 
     return (
         <section id="header" className="header">
-            <nav className="navbar navbar-expand-xl">
+            <nav className="navbar navbar-expand-xl pt-xl-5">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">
-                        <img src={ VertixWeb } alt="" className="vertix-logo me-1 user-select-none" width="60px" height="60px"></img>
-                        <h4><span>V</span>ertix</h4>
+                        <img id="vertix-logo" className="me-1 user-select-none" width="60px" height="60px"
+                             src={ VertixWeb } alt=""></img>
+                        <span id="vertix-brand-logo" className="d-inline">
+                            <span id="vertix-first-letter">V</span>ertix
+                        </span>
                     </a>
 
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -115,21 +118,21 @@ export default function Header() {
 
                         <div className="buttons d-flex p-sm-2">
                             <button id="add-to-server" onClick={ () => onAddToServerClick() }
-                                    className="btn ps-4 pe-4 btn-responsive btn-outline-primary btn-effect me-4">Invite
+                                    className="btn ps-4 pe-4 btn-outline-primary btn-effect me-4">Invite
                                 Vertix
                             </button>
                             <button id="support" onClick={ () => window.open( "https://discord.gg/dEwKeQefUU" ) }
-                                    className="btn btn-responsive btn-outline-success btn-effect ms-auto">Support
+                                    className="btn btn-outline-success btn-effect ms-auto">Support
                             </button>
                         </div>
                     </div>
                 </div>
             </nav>
 
-            <div className="d-flex justify-content-center">
-                <div className={ `wings text-center` }>
-                    <h1 className="user-select-none">Vertix</h1>
-                    <img src={ VertixWeb } alt="" className="vertix-logo user-select-none" width="60" height="60"></img>
+            <div id="vertix-wings" className="d-flex justify-content-center">
+                <div id="vertix-wings-placeholder" className="text-center">
+                    <h1 id="vertix-wings-heading" className="user-select-none">Vertix</h1>
+                    <img id="vertix-wings-logo" className="user-select-none" width="60" height="60" src={ VertixWeb } alt=""></img>
                 </div>
             </div>
         </section>
