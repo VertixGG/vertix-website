3388705874f9b652073f8a9e38868083b86e0555
Leonid Vinikov <czf.leo123@gmail.com>
2023-08-23 07:52:37 +0300

    Tweak: Pages/Changelog - Read from remote instead of using local copy

--- src/vertix/ui/loading-container.tsx
+++ src/vertix/ui/loading-container.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 
-export default function LoadingContainer() {
+export enum LoadingContainerType {
+    PRIMARY = "primary",
+    SECONDARY = "secondary",
+    SUCCESS = "success",
+    DANGER = "danger",
+    WARNING = "warning",
+}
+
+export default function LoadingContainer(
+    props: {
+        type?: LoadingContainerType
+    } = {
+        type: LoadingContainerType.PRIMARY
+    }
+) {
+    const className = `spinner spinner-border text-${ props.type }`;
+
     return (
         <div className="container box-1">
             <div className="row">
                 <div className="col-12 text-center">
-                    <div className="spinner spinner-border text-primary" style={ { width: "100px", height: "100px" } }
-                         role="status">
+                    <div className={ className } style={ { width: "100px", height: "100px" } } role="status">
                     </div>
                 </div>
             </div>
